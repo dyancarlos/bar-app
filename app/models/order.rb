5cf46e31448d2913c1b6bc6e5b2e779fb8085d14
Dyan Carra <dyancarra@gmail.com>
2015-11-14 11:42:35 -0200

    add order

---
+++ app/models/order.rb
@@ -0,0 +1,7 @@
+class Order
+  include Mongoid::Document
+
+  field :payed, type: Boolean, default: ->{ false }
+  field :created_at, type: DateTime, default: ->{ Time.now }
+  field :items, type: Array
+end
