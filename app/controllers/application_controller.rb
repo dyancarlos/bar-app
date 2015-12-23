68330896b8363cfaaa841d3f0344f11a9bf29950
Dyan Carra <dyancarra@gmail.com>
2015-12-22 22:21:47 -0200

    creating

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :create_order_session
+  
+  private
+  def create_order_session
+    if params[:table_id]
+      session[:order] = Order.new(table_id: params[:table_id])
+    end
+  end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,15 +5,15 @@ class OrdersController < ApplicationController
   end
 
   def create
-    Order.create(session[:json])
-    redirect_to :new_order
+    Order.create(session[:order])
+    redirect_to :tables
   end
 
   def add
     @product    = Product.find(params[:id])
     @identifier = rand(1000)
 
-    session[:json]["items"] << {
+    session[:order]["items"] << {
       id: @identifier,
       name: @product.name,
       price: @product.price
@@ -25,7 +25,7 @@ class OrdersController < ApplicationController
   def remove
     @id = params[:id].to_i
 
-    session[:json]["items"] = session[:json]["items"].reject { |r| r["id"] == @id }
+    session[:order]["items"] = session[:order]["items"].reject { |r| r["id"] == @id }
 
     respond_to :js
   end
