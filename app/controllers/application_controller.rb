7f6e619eb85cfe72d78b7804a7241c5bcdff4b56
Dyan Carra <dyancarra@gmail.com>
2015-12-22 22:48:52 -0200

    Verifica se a mesa ja possui um pedido em aberto, caso contrario, instancia um novo objeto (nao esta atualizando ainda)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,24 @@ class ApplicationController < ActionController::Base
   before_filter :create_order_session
   
   private
+  # define order session
+  #
   def create_order_session
     if params[:table_id]
-      session[:order] = Order.new(table_id: params[:table_id])
+      session[:order] = already_have_order?(params[:table_id])
     end
+  end
+  
+  # verify if table already have a not payed order
+  #
+  def already_have_order?(table_id)
+    table = Table.where(id: table_id).first
+    
+    unless table.orders.blank?
+      unless table.orders.last.payed
+        return table.orders.last
+      end
+    end
+    return Order.new(table_id: table_id)
   end
 end
