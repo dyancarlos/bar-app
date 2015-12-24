cfe11145ef9cd931af00506c8e78c9d091d86b9c
Dyan Carra <dyancarra@gmail.com>
2015-12-24 16:36:53 -0200

    Add "fechar mesa"

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,9 @@ class OrdersController < ApplicationController
   def new
   end
 
+  # Create or update order
+  #
+  # TODO Melhorar Isso
   def create
     order = Order.where(id: session[:order]["_id"]["$oid"]).first
 
@@ -14,6 +17,20 @@ class OrdersController < ApplicationController
     redirect_to :root
   end
 
+  # Change order status(payed => true)
+  #
+  def pay
+    order = Order.where(id: session[:order]["_id"]["$oid"]).first
+
+    unless order.blank?
+      order.update_attributes(payed: true)
+    end
+
+    redirect_to :root
+  end
+
+  # Add product to JSON
+  #
   def add
     @product    = Product.find(params[:id])
     @identifier = rand(1000)
@@ -27,6 +44,8 @@ class OrdersController < ApplicationController
     respond_to :js
   end
 
+  # Remove product from JSON
+  #
   def remove
     @id = params[:id].to_i
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     collection do
       get :add
       get :remove
+      get :pay
     end
   end
 end
