ace1623cb75b0fa351e52f6c98df909ece5a5d7c
Dyan Carra <dyancarra@gmail.com>
2015-12-24 22:01:01 -0200

    colorindo mesas

--- app/models/table.rb
+++ app/models/table.rb
@@ -5,4 +5,15 @@ class Table
 
   field :customId, type: Integer
   field :seats, type: Integer, default: ->{ 4 }
+
+  # Return the table color
+  #
+  def return_table_color
+    unless self.orders.blank?
+      unless self.orders.last.payed
+        return "#B02B2C"
+      end
+    end
+    return "#73880A"
+  end
 end
