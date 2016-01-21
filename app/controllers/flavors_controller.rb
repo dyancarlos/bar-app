d449c55029fd247c2c565ffe93bed000d8d80f79
Dyan Carra <dyancarra@gmail.com>
2016-01-21 14:09:49 -0200

    crud flavors

--- app/controllers/flavors_controller.rb
+++ app/controllers/flavors_controller.rb
@@ -1,13 +1,32 @@
 class FlavorsController < ApplicationController
-  layout "admin", only: :new
+  layout "admin", only: [:new, :edit]
 
   def new
-    @flavor = Flavor.new
+    @flavor  = Flavor.new
+    @flavors = Flavor.all.order(name: 1)
+  end
+
+  def edit
+    @flavor = Flavor.find(params[:id])
+  end
+
+  def update
+    @flavor = Flavor.find(params[:id])
+    if @flavor.update_attributes(flavor_params)
+      redirect_to :new_flavor
+    end
   end
 
   def create
     @flavor = Flavor.create(flavor_params)
     if @flavor
+      redirect_to :new_flavor
+    end
+  end
+
+  def destroy
+    @flavor = Flavor.find(params[:id])
+    if @flavor.destroy
       redirect_to :new_flavor
     end
   end
