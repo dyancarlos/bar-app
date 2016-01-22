c42568c998e982a3e904a91b298e94c1ff8dcab2
Dyan Carra <dyancarra@gmail.com>
2016-01-22 14:55:11 -0200

    refactoring order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,15 +1,15 @@
 class Order
   include Mongoid::Document
 
-  belongs_to :table
+  PIZZA_PRICES  = { "P" => 20, "M" => 31, "G" => 36 }
+  BORDER_PRICES = { "P" => 3, "M" => 4, "G" => 5 }
+  SPECIAL_PRICE = 6
 
   field :payed, type: Boolean, default: ->{ false }
   field :created_at, type: DateTime, default: ->{ Time.now }
   field :items, type: Array, default: -> { [] }
 
-  PIZZA_PRICES  = { "P" => 20.0, "M" => 31.0, "G" => 36.0 }
-  BORDER_PRICES = { "P" => 3, "M" => 4, "G" => 5 }
-  SPECIAL_PRICE = 6
+  belongs_to :table
 
   def self.calculate_price(size, border, special)
     pizza  = PIZZA_PRICES[size]
