26c20ea12c88d43d6c6390554b942b22a3f84044
Dyan Carra <dyancarra@gmail.com>
2016-01-22 16:02:09 -0200

    refatoracao

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.where(category_id: params[:id])
+    @flavors  = Flavor.all.order(special: 1)
   end
 
   def new
