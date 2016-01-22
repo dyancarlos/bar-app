8a7ae5873801d8e8caa2f97a640819c3c1f64e4b
Dyan Carra <dyancarra@gmail.com>
2016-01-22 15:21:08 -0200

    colorindo doces e especiais

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
   def count_items
     session[:order]["items"].size
   end
+
+  def special?(obj)
+    "special" if obj
+  end
+
+  def swet?(obj)
+    "swet" if obj
+  end
 end
