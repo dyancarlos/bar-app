11b466d77e84a623748230c8be6e6c2ebff6dc16
Dyan Carra <dyancarra@gmail.com>
2016-03-04 12:17:42 -0300

    ajustes

--- app/controllers/shower_controller.rb
+++ app/controllers/shower_controller.rb
@@ -7,6 +7,16 @@ class ShowerController < ApplicationController
     @type = params[:type]
     orders = Order.all.order_by(created_at: 'desc')
 
-    @orders = orders.select {|d| d.items.delete_if {|c| c["type"] != @type }}
+    if @type == "Pizzas"
+      @orders = orders.select {|d| d.items.delete_if {|c| c["type"] == "Bebidas" or c["type"] == "Petiscos"}}
+    end
+
+    if @type == "Bebidas"
+      @orders = orders.select {|d| d.items.delete_if {|c| c["type"] != "Bebidas"}}
+    end
+
+    if @type == "Petiscos"
+      @orders = orders.select {|d| d.items.delete_if {|c| c["type"] != "Petiscos"}}
+    end
   end
 end
