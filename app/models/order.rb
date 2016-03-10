ab84d72dee06c154582bd79879ed0f4f6f3e750f
Dyan Carra <dyancarra@gmail.com>
2016-03-10 20:43:43 -0300

    melhorias

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,11 @@
 class Order
   include Mongoid::Document
 
-  PIZZA_PRICES  = { "P" => 20.0, "M" => 31.0, "G" => 36.0 }
-  BORDER_PRICES = { "P" => 3.0, "M" => 4.0, "G" => 5.0 }
-  SPECIAL_PRICE = 6
+  PIZZA_PRICES   = { "P" => 20.0, "M" => 31.0, "G" => 36.0 }
+  BORDER_PRICES  = { "P" => 3.0, "M" => 4.0, "G" => 5.0 }
+  SPECIAL_PRICE  = 6
+  GREEN_SECONDS  = 300
+  ORANGE_SECONDS = 600
 
   field :payed, type: Boolean, default: ->{ false }
   field :created_at, type: DateTime, default: ->{ Time.now }
@@ -34,9 +36,9 @@ class Order
   def shower_color
     seconds = (Time.now - self.updated_at).to_i
 
-    if seconds <= 300
+    if seconds <= GREEN_SECONDS
       return "#D9FF8F"
-    elsif seconds > 300 and seconds <= 600
+    elsif seconds > GREEN_SECONDS and seconds <= ORANGE_SECONDS
       return "#FFD48A"
     else
       return "#FF877C"
